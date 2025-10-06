592a962c056b88272daa584eb50cda36a876c327
Riku Sahu <riku.sahu237@gmail.com>
2025-10-07 00:58:44 +0530

    Add files via upload

---
+++ Chapter 1 – Basic/03_circle_area_cylinder_volume.c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int r = 5 , h= 6;
+    printf("Area of a CIRCLE radius %d is %f\n", r, 3.14*r*r);
+
+    printf("Volume of a cylinder having radius %d and height %d is %f", r, h ,3.14*r*r*h );
+    return 0;
+}
