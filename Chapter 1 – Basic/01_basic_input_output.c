da384bb470c87151c24ce142eb5368e845fe4d50
Riku Sahu <riku.sahu237@gmail.com>
2025-10-07 00:55:33 +0530

    Create 01_basic_input_output.c

---
+++ Chapter 1 – Basic/01_basic_input_output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+     /*int a=6 ;
+    printf("The no is %d" , a); */
+
+    int r ;
+    printf("Enter a number : ");
+    scanf("%d" , &r);
+    printf("The no u think is : %d\n", r);
+    return 0;
+}
