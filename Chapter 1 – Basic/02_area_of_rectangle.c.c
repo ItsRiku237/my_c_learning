f7fe589a001d9e73f9556e8c790df9991140fae3
Riku Sahu <riku.sahu237@gmail.com>
2025-10-07 00:57:30 +0530

    Add files via upload

---
+++ Chapter 1 – Basic/02_area_of_rectangle.c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+   /* int l= 4, b= 6 ;
+    printf("Area of the rectangle of length %d and breath %d is %d \n" , l , b , l*b);*/
+
+    int l , b ;
+    printf("Enter length:");
+    scanf("%d", &l);
+    printf("Enter breath:");
+    scanf("%d" , &b);
+    printf("Area of the rectangle of length %d and breath %d is %d \n" , l , b , l*b);
+    return 0;
+
+}
