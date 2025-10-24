7c41d19fa4ce4b74bb51fd33a9527c94fb8066f2
Riku Sahu <riku.sahu237@gmail.com>
2025-10-24 22:26:43 +0530

    Add files via upload

---
+++ Chapter 10 - Dynamic_memory_allocation/07_dynamic_array_calloc.c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Write a program to dynamically create an array of size 6 capable of storing 6  integers.
+// Use the array in problem 1 to store 6 integers entered by the user.
+// Solve problem 1 using calloc(). 
+int main()
+{
+    int n = 5;
+    int *ptr;
+    ptr = (int *)calloc(n, sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d\n", &ptr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", ptr[i]);
+    }
+
+    return 0;
+}
